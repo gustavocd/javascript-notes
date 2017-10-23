6fe1582aaf7289f9fb599eeb687f4435d8478768
gustavocd <gustavocastillo1993@hotmail.com>
2017-10-22 22:37:42 -0500

    Add output example to the bubbleSort function when it is executed

--- algorithms/sort/bubbleSort.js
+++ algorithms/sort/bubbleSort.js
@@ -15,6 +15,6 @@ const numbers = [1, 5, 3, 2, 1, 0, 10]
 const heights = [1.79, 1.64, 1.45, 1.89, 2.10, 1.20]
 const years = [2001, 2000, 1993, 1990, 1964, 2017, 2016]
 
-console.log(bubbleSort(numbers))
-console.log(bubbleSort(years))
-console.log(bubbleSort(heights))
+console.log(bubbleSort(numbers)) // [ 0, 1, 1, 2, 3, 5, 10 ]
+console.log(bubbleSort(heights)) // [ 1.2, 1.45, 1.64, 1.79, 1.89, 2.1 ]
+console.log(bubbleSort(years)) // [ 1964, 1990, 1993, 2000, 2001, 2016, 2017 ]
